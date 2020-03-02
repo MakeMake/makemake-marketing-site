29bde308f72e09404ba33123d809fe5ca32747e0
Mickael Marquez <marquez.mickael@gmail.com>
2020-03-02 17:26:51 +0100

    tiles

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -6,9 +6,11 @@
 import 'core-js';
 import 'regenerator-runtime/runtime';
 import $ from 'jquery';
+import feather from 'feather-icons'
 import { Link } from '../_modules/link/link';
 
 $(() => {
   new Link(); // Activate Link modules logic
   console.log('Welcome to Yeogurt!');
+  feather.replace()
 });
