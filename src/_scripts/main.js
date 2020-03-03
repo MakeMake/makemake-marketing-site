1c5065cc75f2eda3749fa1c9630958b679ccc949
Mickael Marquez <marquez.mickael@gmail.com>
2020-03-03 13:46:31 +0100

    animated anchor link

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -13,4 +13,17 @@ $(() => {
   new Link(); // Activate Link modules logic
   console.log('Welcome to Yeogurt!');
   feather.replace()
+
+  // anchor link animation
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          block: 'center',
+          behavior: 'smooth'
+      });
+    });
+  });
+
 });
